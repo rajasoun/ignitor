b8d2964fa0127412de5cc31f07e1f4b7d1f5ddd6
Raja. S <rajasoun@icloud.com>
2017-02-21 09:05:15 +0000

    Import Seed Data

--- backbone/workers/mysqlseed.sh
+++ backbone/workers/mysqlseed.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 
-docker exec -it ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b  < /opt/data/seed.sql'
+docker exec -it ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b  < /opt/initdb/seed.sql'
