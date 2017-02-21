67bbd9fa078eab9b52cfa16fdd7222fc1d11d4ba
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-21 13:35:00 +0530

    Simplify Ops Framework

--- backbone/workers/mysqlseed.sh
+++ backbone/workers/mysqlseed.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
 
-docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e "CREATE DATABASE IF NOT EXISTS b2b;"
-docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e "CREATE user ckuser identified by 'Cisco@1234';"
-docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e "delete from resource;"
-docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e "delete from subject;"
+docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e  "CREATE DATABASE b2b;"
+docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e  "CREATE USER 'ckuser'@'localhost' IDENTIFIED BY 'Cisco@1234';"
+docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e  "GRANT ALL PRIVILEGES ON * . * TO 'ckuser'@'localhost';"
+docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e  "CREATE USER 'ckuser'@'%' IDENTIFIED BY 'Cisco@1234';"
+docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e  "GRANT ALL PRIVILEGES ON * . * TO 'ckuser'@'%';"
+
+docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e "DELETE FROM RESOURCE;"
+docker exec -it ckdb mysql  -u ckuser -pCisco@1234 b2b -e "DELETE FROM SUBJECT;"
