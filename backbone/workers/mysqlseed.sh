86a769d3f31b33ebc4b63a8a1b5c6f92d7014e47
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-23 10:02:36 +0530

    Simplify Ops Framework

--- backbone/workers/mysqlseed.sh
+++ backbone/workers/mysqlseed.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
+MARIADB_USER=ckuser
+MARIADB_DATABASE=b2b
 
-docker exec -it ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b  < /opt/initdb/seed.sql'
+echo "Grating access for $MARIADB_USER to $MARIADB_DATABASE"
+docker exec -it ckdb  sh -c  'mysql  -u root -ppassword@1234 "GRANT ALL ON *.* TO '$MARIADB_USER'@'%' ;FLUSH PRIVILEGES;"'
+docker exec -it ckdb  sh -c   "GRANT ALL ON *.* TO '$MARIADB_USER'@'localhost' ;FLUSH PRIVILEGES;"
+docker exec -it ckdb  sh -c   "GRANT ALL ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' ;FLUSH PRIVILEGES;"
+docker exec -it ckdb  sh -c   "GRANT ALL ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'localhost' ;FLUSH PRIVILEGES;"
 
+echo "Schema Creation For Auth"
+docker exec -it ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b  < initdb/seed.sql'
 docker exec -it ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b -e "show tables;"'
