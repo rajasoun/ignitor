c3c34cb533638aa30f49ee9c1a710034ddc9dcb9
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-23 10:45:50 +0530

    Simplify Ops Framework

--- backbone/workers/mysqlseed.sh
+++ backbone/workers/mysqlseed.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 echo "Grant Privilages & Schema Creation For Auth"
-docker exec -it -v $(pwd):/app ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b  < /app/initdb/seed.sql'
+docker exec -it -v $(pwd):/app ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b  < /initdb/seed.sql'
 docker exec -it ckdb  sh -c  'mysql  -u ckuser -pCisco@1234 b2b -e "show tables;"'
