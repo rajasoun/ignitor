939a8f11bafdbd7c06266ad9997d842a2e2be2ed
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-23 13:26:26 +0530

    Simplify Ops Framework

--- backbone/workers/mongoseed.sh
+++ backbone/workers/mongoseed.sh
@@ -2,10 +2,11 @@
 
 #FIX: http://stackoverflow.com/questions/29415311/mongodb-connection-auth-works-on-shell-but-not-with-external-tools
 
-docker exec -it ckmongo  sh -c 'mongo admin <<EOF
-db.admin.system.users.remove({})    <== removing all users
-db.admin.system.version.remove({}) <== removing current version
-db.admin.system.version.insert({ "_id" : "authSchema", "currentVersion" : 3 })
+docker exec -it ckmongo  sh  -c 'mongo admin 2>/dev/null <<EOF
+use admin
+db.system.users.remove({})
+db.system.version.remove({})
+db.system.version.insert({ "_id" : "authSchema", "currentVersion" : 3 })
 '
 
 docker exec -it ckmongo  sh -c 'mongo b2b <<EOF
