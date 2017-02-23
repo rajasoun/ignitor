2be1b458b8b1501b2476d579482e426d9a57e4a8
Raja. S <rajasoun@icloud.com>
2017-02-23 06:23:39 +0000

    Simplify Ops Framework

--- backbone/start.sh
+++ backbone/start.sh
@@ -3,6 +3,7 @@
 cd base && make && cd ..
 cd java && make && cd ..
 docker-compose build
+docker volume ls -qf dangling=true | xargs -r docker volume rm
 docker-compose run --rm start_dependencies
 docker-compose up -d
 python fcci/app.py &
