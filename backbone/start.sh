0d85b37ab995c3805834e15706a96e304c458212
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-23 23:52:04 +0530

    Simplify Ops Framework

--- backbone/start.sh
+++ backbone/start.sh
@@ -5,6 +5,8 @@ cd java && make && cd ..
 docker-compose build
 docker volume ls -qf dangling=true | xargs -r docker volume rm
 docker-compose run --rm start_dependencies
+sh -c "workers/mysqlseed.sh"
+sh -c "workers/mongoseed.sh"
 docker-compose up -d
 python fcci/app.py &
 
