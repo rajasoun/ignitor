58f10e8105bf11ef6947156413244be886c8834c
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-23 19:40:27 +0530

    Simplify Ops Framework

--- backbone/workers/scripts/start.sh
+++ backbone/workers/scripts/start.sh
@@ -10,7 +10,7 @@ app=$3
 
 echo "Starting $app ...."
 export JAVA_OPTS="-Xms512m -Xmx1024m -javaagent:$server_target/target/aspectjweaver-1.7.4.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8202"
-echo "java ${JAVA_OPTS} -cp $CLASSPATH:$server_target/target/server-jar-with-dependencies.jar $jetty $port $war $app"
+java ${JAVA_OPTS} -cp $CLASSPATH:$server_target/target/server-jar-with-dependencies.jar $jetty $port $war $app
 echo "***Done***"
 
 
