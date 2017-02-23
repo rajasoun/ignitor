05ebb07cd24b8b1e48e8d3922ee28f03839380c8
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-23 10:51:49 +0530

    Simplify Ops Framework

--- backbone/workers/initdb/grant.sql
+++ backbone/workers/initdb/grant.sql
@@ -1,5 +1,3 @@
 GRANT ALL ON *.* TO 'ckuser'@'%' ;FLUSH PRIVILEGES;
-GRANT ALL ON *.* TO 'ckuser'@'localhost' ;FLUSH PRIVILEGES;
 GRANT ALL ON b2b.* TO 'ckuser'@'%' ;FLUSH PRIVILEGES;
-GRANT ALL ON b2b.* TO 'ckuser'@'localhost' ;FLUSH PRIVILEGES;
 
