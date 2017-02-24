791566983d2de5bb48b59dc51eb332ce729f517a
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-24 13:53:30 +0530

    Simplify Ops Framework

--- backbone/workers/scripts/start.sh
+++ backbone/workers/scripts/start.sh
@@ -8,13 +8,16 @@ war="$1-web/target/$3.war"
 app=$3
 
 if [ "$server_target" == "tm" ]; then
-	jetty="com.cisco.ccl.b2b.$1.server.JettyServer"
-else
 	jetty="com.cisco.ccl.b2b.$3.server.JettyServer"
+else
+	jetty="com.cisco.ccl.b2b.$1.server.JettyServer"
 fi
 
 echo "Starting $app ...."
 export JAVA_OPTS="-Xms512m -Xmx1024m -javaagent:$server_target/target/aspectjweaver-1.7.4.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8202"
+echo "+++"
+echo "java ${JAVA_OPTS} -cp $CLASSPATH:$server_target/target/server-jar-with-dependencies.jar $jetty $port $war $app "
+echo "+++"
 java ${JAVA_OPTS} -cp $CLASSPATH:$server_target/target/server-jar-with-dependencies.jar $jetty $port $war $app
 echo "***Done***"
 
