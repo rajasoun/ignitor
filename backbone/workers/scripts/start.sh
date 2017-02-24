3d94b51c352b522c5052b5de3bb80c55d61deb5b
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-24 13:43:10 +0530

    Simplify Ops Framework

--- backbone/workers/scripts/start.sh
+++ backbone/workers/scripts/start.sh
@@ -3,10 +3,15 @@
 echo "Switching to Docker App Directory"
 cd /app
 server_target="$1-server"
-jetty="com.cisco.ccl.b2b.$1.server.JettyServer"
 port=$2
 war="$1-web/target/$3.war"
 app=$3
+
+if [ "$server_target" == "tm" ]; then
+	jetty="com.cisco.ccl.b2b.$1.server.JettyServer"
+else
+	jetty="com.cisco.ccl.b2b.$3.server.JettyServer"
+fi
 
 echo "Starting $app ...."
 export JAVA_OPTS="-Xms512m -Xmx1024m -javaagent:$server_target/target/aspectjweaver-1.7.4.jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8202"
