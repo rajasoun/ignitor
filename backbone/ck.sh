b4119c5a166dcdfdc20f6f469d71b37dba10e9bf
Raja. Soundaramourty <rajasoun@icloud.com>
2017-02-24 15:30:48 +0530

    Simplify Ops Framework

--- backbone/ck.sh
+++ backbone/ck.sh
@@ -19,6 +19,7 @@ case "$option" in
         docker-compose -f $composer  up -d
         /usr/bin/python  fcci/app.py &
         docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro spotify/docker-gc
+        docker volume ls -qf dangling=true | xargs -r docker volume rm
     ;;
     stop)
         echo -n "Starting $DESC: "
@@ -27,6 +28,7 @@ case "$option" in
         ps -ef | grep '[p]ython fcci/app.py' | awk '{ print $2 }' | xargs kill
         sleep 2
         echo "fcci/app.py killed."
+        docker volume ls -qf dangling=true | xargs -r docker volume rm
     ;;
     *)
         echo "Usage: ./ck.sh {self-hosted.yml|third-party.yml} {start|stop}" >&2
