3c486427b5af75e5bd00baa6574be151d83df95d
Raja. Soundaramourty <rajasoun@icloud.com>
2017-03-04 14:10:54 +0530

    [US14374]  Dev Env Initial Setup Update

--- backbone/workers/setupdev.sh
+++ backbone/workers/setupdev.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 
-tenant=dev.xkit.co-2c7b663a12d39a6bbd4f062854705f57
+tenant="dev.xkit.co-2c7b663a12d39a6bbd4f062854705f57"
 curl -XDELETE localhost:8006/authorization/tenant/$tenant/notification
 #curl -XDELETE localhost:9200/$tenant-b2b-kc-resources
+
+curl -XPUT   https://dev.xkit.co/knowledgecenter/tenants/dev.xkit.co -H"Content-Type:application/json" -d
+'
+    {"_id": $tenant}
+'
 
 print_message "Onboarding tenant $tenant"
 curl -XPOST http://localhost:8006/authorization/tenant/$tenant/notification -H"Content-Type:application/json" -d ''
