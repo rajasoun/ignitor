7f915c026c72eb33ed2f002390e7bdfe3e7ce9dc
Raja. Soundaramourty <rajasoun@icloud.com>
2017-03-29 13:59:50 +0530

    Go URL Monitor

--- backbone/url-monitor/entrypoint.sh
+++ backbone/url-monitor/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 
-apk add --no-cache bash git openssh
+apk add --no-cache git
 go get "github.com/Sirupsen/logrus"
 go get "github.com/castawaylabs/cachet-monitor"
 go get "github.com/docopt/docopt-go"
 go get "github.com/mitchellh/mapstructure"
 go get "gopkg.in/yaml.v2"
+apk del git
 
 go run main.go -c config.yml
