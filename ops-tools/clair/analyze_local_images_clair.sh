6a564ac96599d1981242984bfcfc074dcfa04554
Raja. Soundaramourty <rajasoun@icloud.com>
2017-03-30 10:28:57 +0530

    [US13686] Add Clair - Container Security Scanner

--- ops-tools/clair/analyze_local_images_clair.sh
+++ ops-tools/clair/analyze_local_images_clair.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run --name="analyze-local-images"  -d  --rm -v `pwd`:/app -w /app  golang:1.8-alpine  /app/entrypoint.sh $1
+docker run --name="analyze-local-images" --rm -v `pwd`:/app -w /app  golang:1.8-alpine  /app/entrypoint.sh $1
