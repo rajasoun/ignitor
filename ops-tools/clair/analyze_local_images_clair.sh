d9a6e47ff8c7a216d201389d90464f31f1a88f03
Raja. Soundaramourty <rajasoun@icloud.com>
2017-03-30 10:39:47 +0530

    [US13686] Add Clair - Container Security Scanner

--- ops-tools/clair/analyze_local_images_clair.sh
+++ ops-tools/clair/analyze_local_images_clair.sh
@@ -2,4 +2,9 @@
 ### https://github.com/coreos/clair/tree/master/contrib/analyze-local-images
 # Go To Be Available On Local System
 # go get -u github.com/coreos/clair/contrib/analyze-local-images
-analyze-local-images  $1
+images=$(docker images -q)
+
+for image in $images
+do
+    analyze-local-images  $image
+done
