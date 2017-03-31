5a4be895c51cdb88b375f7405de862e4d3131f35
Raja. Soundaramourty <rajasoun@icloud.com>
2017-03-31 09:48:19 +0530

    [US13686] Cachet & Security Tools Refactoring

--- ops-tools/cachet/client/configure_components.py
+++ ops-tools/cachet/client/configure_components.py
@@ -17,13 +17,11 @@ print(version.get())
 components = cachet.Components(endpoint=ENDPOINT, api_token=API_TOKEN)
 portainer = json.loads(components.post(name='Portainer',
                                        status=1,
-                                       id =2,
-                                       link='http://dev.xkit.co:9000/',
+                                       link='http://dev.xkit.co:9000/portainer',
                                        description='Portainer'))
 
 cachet = json.loads(components.post(name='cachet',
                                        status=1,
-                                       id =3,
                                        link='http://dev.xkit.co:8787/',
                                        description='cachet'))
 
