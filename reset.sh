da61a552e4e3fbd6c98599b6d53b2d1274307d21
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-01 17:17:01 +0530

    [US13686] Manual  Nginx Proxy

--- reset.sh
+++ reset.sh
@@ -3,6 +3,4 @@
 sh -c "tools/ops/clean/docker-clean all"
 docker-compose -f  tools/ops/portainer/portainer.yml  build
 docker-compose -f  tools/ops/portainer/portainer.yml  up -d
-cd web-proxy
-sh -c "proxy.sh setup"
-sh -c "proxy.sh start"
+
