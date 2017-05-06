9a75ddd3c98fd55ad199bc8942c25176bf5fbb65
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-06 21:41:05 +0530

    [US13686] Manual  Nginx Proxy with Hugo Based Site

--- reset.sh
+++ reset.sh
@@ -3,5 +3,10 @@
 sh -c "tools/ops/clean/docker-clean all"
 docker-compose -f  tools/ops/portainer/portainer.yml  build
 docker-compose -f  tools/ops/portainer/portainer.yml  up -d
-docker-compose -f  tools/ops/portainer/portainer.yml  up -d
+
+sh -c "web-proxy/proxy.sh setup"
+sh -c "web-proxy/proxy.sh start"
+
+sh -c "docs/docs.sh setup"
+sh -c "docs/docs.sh start"
 
