9bc01fd07c386e28f39ed0f2bfbbd0c4b2a61fcd
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-06 15:02:47 +0530

    Tracker Docs

--- docs/tracker/deploy.sh
+++ docs/tracker/deploy.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-rm -fr public
+
 # Build the index.
 npm install
 npm run index
 
-git clone add https://github.com/rajasoun/tracker.doc public
 # Build the project.
 hugo
 
-cd public
 # Add changes to git.
 git add -A
 
@@ -23,4 +21,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-cd -
+git subtree push --prefix=public git@github.com:rajasoun/tracker.doc.git gh-pages
