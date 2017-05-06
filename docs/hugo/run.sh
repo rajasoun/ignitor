ba362c0aa3316bef3e35b8cd6e66a399c3ba3890
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-06 22:13:06 +0530

    [US13686] Manual  Nginx Proxy with Hugo Based Site

--- docs/hugo/run.sh
+++ docs/hugo/run.sh
@@ -14,10 +14,10 @@ while [ true ]
 do
     if [[ $HUGO_WATCH != 'false' ]]; then
 	    echo "Watching..."
-        $HUGO server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --bind="0.0.0.0" "$@" || exit 1
+        $HUGO server --watch=true --source="/app" --theme="$HUGO_THEME" --destination="/app/public" --baseUrl="$HUGO_BASEURL" --bind="0.0.0.0" "$@" || exit 1
     else
 	    echo "Building one time..."
-        $HUGO --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" "$@" || exit 1
+        $HUGO --source="/app" --theme="$HUGO_THEME" --destination="/app/public" --baseUrl="$HUGO_BASEURL" "$@" || exit 1
     fi
 
     if [[ $HUGO_REFRESH_TIME == -1 ]]; then
