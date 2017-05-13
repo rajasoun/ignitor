ac2f7d585600aa1d98a6ffa2e2b7fd6b81f909ee
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-13 10:43:26 +0530

    [US13686] Nginx Static Container For Tracker (Task Rocket)

--- reset.sh
+++ reset.sh
@@ -19,3 +19,8 @@ docker-compose -f  tools/ops/tracker/tracker.yml  up -d --build
 docker-compose -f web-proxy/proxy.yml up -d --build
 
 
+## Unified Log
+docker run -d --name="log-ck" --rm --volume=/var/run/docker.sock:/var/run/docker.sock --publish=127.0.0.1:8989:80 gliderlabs/logspout
+
+curl http://127.0.0.1:8989/logs
+
