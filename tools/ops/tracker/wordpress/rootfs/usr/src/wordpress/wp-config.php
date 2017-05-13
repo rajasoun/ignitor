6a926816242cf10d639e64c8831b781ad08f512c
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-13 10:56:54 +0530

    [US13686] Nginx Static Container For Tracker (Task Rocket)

--- tools/ops/tracker/wordpress/rootfs/usr/src/wordpress/wp-config.php
+++ tools/ops/tracker/wordpress/rootfs/usr/src/wordpress/wp-config.php
@@ -16,16 +16,15 @@ if (!defined('ABSPATH')) {
     define('ABSPATH', dirname(__FILE__) . '/');
 }
 
-$_SERVER['HTTPS']='on';
-$_SERVER['REQUEST_URI'] = "/actions".$_SERVER['REQUEST_URI'];
 
-// if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
-//     $_SERVER['HTTPS']='on';
 
-// if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-//     $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
-//     $_SERVER['REQUEST_URI'] = "/actions".$_SERVER['REQUEST_URI'];
-// }
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+    $_SERVER['HTTPS']='on';
+
+if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    $_SERVER['REQUEST_URI'] = "/tasks/".$_SERVER['REQUEST_URI'];
+}
 
 
 require_once(ABSPATH . 'wp-secrets.php');
