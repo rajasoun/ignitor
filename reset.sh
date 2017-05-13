5ca0e887317afc2060872711dfaf86425047d6d8
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-13 10:36:12 +0530

    [US13686] Nginx Static Container For Tracker (Task Rocket)

--- reset.sh
+++ reset.sh
@@ -5,9 +5,6 @@ sh -c "tools/ops/clean/docker-clean all"
 ## Setup hostlocal.io
 docker run --rm --privileged --net=host gliderlabs/hostlocal
 
-## Web Proxy
-docker-compose -f web-proxy/proxy.yml up -d --build
-
 ##Portainer
 docker-compose -f  tools/ops/portainer/portainer.yml  up -d --build
 
@@ -17,5 +14,8 @@ docker-compose -f  docs/docs.yml  up -d --build
 
 ##Portainer
 docker-compose -f  tools/ops/tracker/tracker.yml  up -d --build
+
+## Web Proxy
+docker-compose -f web-proxy/proxy.yml up -d --build
 
 
