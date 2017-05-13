e74886ff0d6e30299e867112434864e22fe5a284
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-13 12:19:42 +0530

    [US13686] Nginx Static Container For Tracker (Task Rocket)

--- tools/ops/tracker/wordpress/rootfs/usr/src/wordpress/wp-config.php
+++ tools/ops/tracker/wordpress/rootfs/usr/src/wordpress/wp-config.php
@@ -21,18 +21,6 @@ if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
     $_SERVER['HTTPS']='on';
 
 if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-echo '<br>Before:';
-echo '<br>$_SERVER[\'HTTP_HOST\'] : ' . $_SERVER['HTTP_HOST'];
-echo '<br>$_SERVER[\'HTTP_X_FORWARDED_HOST\']: ' . $_SERVER['HTTP_X_FORWARDED_HOST'];
-echo '<br>$_SERVER[\'REQUEST_URI\']: ' . $_SERVER['REQUEST_URI'];
-echo '<br>$_SERVER[\'HTTP_X_FORWARDED_SERVER\']: ' . $_SERVER['HTTP_X_FORWARDED_SERVER'];
-echo '<br>$_SERVER[\'HTTP_X_FORWARDED_FOR\']: ' . $_SERVER['HTTP_X_FORWARDED_FOR'];
-echo '<br>$_SERVER[\'HTTP_X_FORWARDED_FOR\']: ' . $_SERVER['HTTP_X_FORWARDED_FOR'];
-echo '<br>$_SERVER[\'HTTPS\']: ' . $_SERVER['HTTPS'];
-echo '<br>$_SERVER[\'REMOTE_ADDR\']: ' . $_SERVER['REMOTE_ADDR'];    
-echo '<br>$_SERVER[\'SERVER_NAME\']: ' . $_SERVER['SERVER_NAME'];
-echo '<br>$_SERVER[\'SERVER_PROTOCOL\']: ' . $_SERVER['SERVER_PROTOCOL']; 
-
     $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
     $_SERVER['REQUEST_URI'] = "/actions".$_SERVER['REQUEST_URI']; 
 }
