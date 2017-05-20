6df7c66900d3f2c8bacd25ff2bed4e3f2633a198
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-20 16:05:33 +0530

    [US13686] Reset Script Fix For VMs

---
+++ tools/ops/tracker/tasks/rootfs/etc/var/www/wordpress/wp-config.php
@@ -0,0 +1,32 @@
+<?php
+
+define('WP_CONTENT_DIR', '/var/www/wp-content');
+define('WP_MEMORY_LIMIT', '64M');
+
+$table_prefix  = getenv('TABLE_PREFIX') ?: 'wp_';
+
+foreach ($_ENV as $key => $value) {
+    $capitalized = strtoupper($key);
+    if (!defined($capitalized)) {
+        define($capitalized, $value);
+    }
+}
+
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+    $_SERVER['HTTPS']='on';
+
+if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    $_SERVER['REQUEST_URI'] = "/tasks".$_SERVER['REQUEST_URI']; 
+}
+
+
+
+
+require_once(ABSPATH . 'wp-secrets.php');
+require_once(ABSPATH . 'wp-settings.php');
