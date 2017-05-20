ea5ba2459154c9c2b50f0e1487b376b705854706
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-20 15:24:15 +0530

    [US13686] Reset Script Fix For VMs

--- reset-common.sh
+++ reset-common.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
 sh -c "tools/ops/clean/docker-clean all"
-
 docker-compose -f  tools/ops/portainer/portainer.yml  up -d --build
-docker-compose -f  docs/docs.yml  up -d --build
+
 
 ## Unified Log
 docker run -d --name="log-ck" --rm --volume=/var/run/docker.sock:/var/run/docker.sock --publish=127.0.0.1:8989:80 gliderlabs/logspout
