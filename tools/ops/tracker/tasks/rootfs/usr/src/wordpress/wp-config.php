e2bce9903d8dad0b218ebc3a5a23ada459a6680b
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-20 16:38:03 +0530

    [US13686] Reset Script Fix For VMs

--- tools/ops/tracker/tasks/rootfs/usr/src/wordpress/wp-config.php
+++ tools/ops/tracker/tasks/rootfs/usr/src/wordpress/wp-config.php
@@ -22,11 +22,8 @@ if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
 
 if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
     $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
-    $_SERVER['REQUEST_URI'] = "/tasks".$_SERVER['REQUEST_URI']; 
+    $_SERVER['REQUEST_URI'] = "/tasks".$_SERVER['REQUEST_URI'];
 }
-
-
-
 
 require_once(ABSPATH . 'wp-secrets.php');
 require_once(ABSPATH . 'wp-settings.php');
