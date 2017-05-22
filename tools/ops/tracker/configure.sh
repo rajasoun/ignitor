10f62d27bbdfcc0aca26d16a07fb861171384d86
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-22 16:47:01 +0530

    [US13686] Doctor App

--- tools/ops/tracker/configure.sh
+++ tools/ops/tracker/configure.sh
@@ -2,10 +2,5 @@
 
 # terminate on errors
 set -e
-docker exec -it tracker_tasks_1 wp core install --path=/usr/src/wordpress/ \
-         --url=https://tracker.learn.cisco/tasks \
-         --title="Tracker" --admin_user=admin \
-         --admin_password=vziMjlWd3^RoZRO%i# \
-         --admin_email=rajasoun@icloud.com  --allow-root
-docker exec -it tracker_tasks_1 wp cli update --path=/usr/src/wordpress/ --allow-root
 
+docker exec -it tracker_tasks_1 wp cli update --path=/usr/src/wordpress/ --allow-root
