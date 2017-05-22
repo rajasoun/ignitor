144ac3d46cdd725c8b4e30f6065fd5eaed35891b
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-22 09:01:49 +0530

    [US13686] Fix For Media Upload Issues

--- tools/ops/tracker/tasks/rootfs/fix-wordpress-permissions.sh
+++ tools/ops/tracker/tasks/rootfs/fix-wordpress-permissions.sh
@@ -22,16 +22,3 @@ find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
 find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
 find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
 
-# Check if volume is empty
-if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
-    echo 'Setting up wp-content volume'
-    # Copy wp-content from Wordpress src to volume
-    cp -r /usr/src/wordpress/wp-content /var/www/
-    chown -R nobody.nobody /var/www
-
-    # Generate secrets
-    curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
-fi
-
-exec "$@"
-
