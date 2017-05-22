f5e95dd05fe6574da4dc33103944b3358367baff
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-22 23:06:40 +0530

    [US13686] WP Permission Fixes - Hip Hip Hurray

--- tools/ops/tracker/configure.sh
+++ tools/ops/tracker/configure.sh
@@ -9,5 +9,6 @@ docker exec -it tracker_tasks_1 wp core install --path=/usr/src/wordpress/ \
          --admin_user=admin \
          --admin_password=vziMjlWd3^RoZRO%i# \
          --admin_email=rajasoun@icloud.com
-
+docker exec -it tracker_tasks_1 /etc/cont-init.d/11-00-set-permissions
 docker exec -it tracker_tasks_1 wp cli update --path=/usr/src/wordpress/ --allow-root
+
