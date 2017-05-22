79dc2d11efa8cfbd6a9949655531dcf9c9b2c34f
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-22 23:05:26 +0530

    [US13686] WP Permission Fixes - Hip Hip Hurray

--- tools/ops/tracker/configure.sh
+++ tools/ops/tracker/configure.sh
@@ -5,8 +5,9 @@ set -e
 
 docker exec -it tracker_tasks_1 wp core install --path=/usr/src/wordpress/ \
          --url=https://tracker.learn.cisco/tasks \
-         --title="Tracker" --admin_user=admin \
+         --title="Tracker" \
+         --admin_user=admin \
          --admin_password=vziMjlWd3^RoZRO%i# \
-         --admin_email=rajasoun@icloud.com  --allow-root
+         --admin_email=rajasoun@icloud.com
 
 docker exec -it tracker_tasks_1 wp cli update --path=/usr/src/wordpress/ --allow-root
--- tools/ops/tracker/tasks/rootfs/usr/src/wordpress/wp-config.php
+++ tools/ops/tracker/tasks/rootfs/usr/src/wordpress/wp-config.php
@@ -26,9 +26,9 @@ if (!defined('ABSPATH')) {
     define('ABSPATH', dirname(__FILE__) . '/');
 }
 
-
-if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
     $_SERVER['HTTPS']='on';
+
 
 if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
     $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
