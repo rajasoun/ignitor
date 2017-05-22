352c86283d46f29f7a03491a9273b50b321b832d
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-22 22:22:46 +0530

    [US13686] WP Permission Fixes [RevertPoint]

--- tools/ops/tracker/tracker.sh
+++ tools/ops/tracker/tracker.sh
@@ -8,18 +8,9 @@ cleanup(){
     docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro spotify/docker-gc
 }
 
-setup_ssl_certificate(){
-    ansible-vault decrypt \
-        ~/Workspace/ck/ignitor/tools/ops/tracker/tasks/rootfs/etc/cont-init.d/11-00-configure-wordpress \
-        --vault-password-file ~/Workspace/ck/ignitor/.vault_pass
-}
 
 setup_hostlocal(){
     docker run --rm --privileged --net=host gliderlabs/hostlocal
-}
-
-teardown_ssl_certificate(){
-    git reset --hard
 }
 
 
@@ -34,10 +25,8 @@ case "$option" in
 
     start)
         echo -n "Starting $DESC: "
-        setup_ssl_certificate ##Secret File  .vault_pass is required
         docker-compose $composer run --rm start_dependencies
         docker-compose $composer up -d  --build
-        teardown_ssl_certificate
     ;;
 
     stop)
