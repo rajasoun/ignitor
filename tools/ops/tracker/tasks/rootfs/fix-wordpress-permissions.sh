8ee409a63a8a01d2c6255c251779c26f9d88e687
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-22 08:58:49 +0530

    [US13686] Fix For Media Upload Issues

--- tools/ops/tracker/tasks/rootfs/fix-wordpress-permissions.sh
+++ tools/ops/tracker/tasks/rootfs/fix-wordpress-permissions.sh
@@ -7,7 +7,6 @@ WP_OWNER=nobody # <-- wordpress owner
 WP_GROUP=nobody # <-- wordpress group
 WP_ROOT=/usr/src/wordpress # <-- wordpress root directory
 WS_GROUP=nginx # <-- webserver group
-WS_DATA=/var/www
 
 # reset to safe defaults
 find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
@@ -18,18 +17,17 @@ find ${WP_ROOT} -type f -exec chmod 644 {} \;
 chgrp ${WS_GROUP} ${WP_ROOT}/wp-config.php
 chmod 660 ${WP_ROOT}/wp-config.php
 
+# allow wordpress to manage wp-content
+find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
+find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
+find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
+
 # Check if volume is empty
 if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
     echo 'Setting up wp-content volume'
     # Copy wp-content from Wordpress src to volume
     cp -r /usr/src/wordpress/wp-content /var/www/
-
-    # allow wordpress to manage wp-content
-    find ${WS_DATA}/wp-content -exec chgrp ${WS_GROUP} {} \;
-    find ${WS_DATA}/wp-content -type d -exec chmod 775 {} \;
-    find ${WS_DATA}/wp-content -type f -exec chmod 664 {} \;
-
-    #chown -R nobody.nobody /var/www
+    chown -R nobody.nobody /var/www
 
     # Generate secrets
     curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
