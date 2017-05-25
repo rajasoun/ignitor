aed1799782c308e561c99ebea0dfb1f45091c229
Raja. Soundaramourty <rajasoun@icloud.com>
2017-05-25 13:53:50 +0530

    [US13686] Fix Java Version Upgrade & Doctor App

--- tools/ops/tracker/tasks/config/wp-config.php
+++ tools/ops/tracker/tasks/config/wp-config.php
@@ -41,5 +41,4 @@ if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
     $_SERVER['REQUEST_URI'] = "/tasks".$_SERVER['REQUEST_URI'];
 }
 
-require_once(ABSPATH . 'wp-secrets.php');
 require_once(ABSPATH . 'wp-settings.php');
