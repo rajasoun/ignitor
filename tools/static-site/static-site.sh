e6af1a8e80c47e1be87d0ef329cebea54cb49670
Raja. Soundaramourty <rajasoun@icloud.com>
2017-06-05 10:42:09 +0530

    [US13686] Nginx Refactoring

---
+++ tools/static-site/static-site.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+
+option=$1
+composer="-f tracker.learn.cisco.yml"
+DESC="Tracker Static Site"
+
+cleanup(){
+    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro spotify/docker-gc
+}
+
+
+setup_hostlocal(){
+    docker run --rm --privileged --net=host gliderlabs/hostlocal
+}
+
+
+
+set -e
+
+case "$option" in
+    setup)
+       echo -n "Setup $DESC "
+       setup_hostlocal #To Enable hostlocal.io
+    ;;
+
+    start)
+        echo -n "Starting $DESC: "
+        docker-compose $composer up -d  --build
+    ;;
+
+    stop)
+        echo -n "Stopping $DESC: "
+        docker-compose $composer down
+    ;;
+
+    teardown)
+        echo -n "TearDown $DESC: "
+        docker-compose $composer down
+        cleanup
+    ;;
+
+    log)
+        docker-compose $composer logs -f
+    ;;
+    *)
+        echo "Usage: ./static-site.sh {setup|start|stop|teardown|log}" >&2
+        exit 1
+    ;;
+esac
+exit 0
