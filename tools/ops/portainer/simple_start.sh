4ce420addc590152ffc3dd575c59d72f146180c8
Raja. Soundaramourty <rajasoun@icloud.com>
2017-06-08 11:46:59 +0530

    [US13686] Nginx Refactoring using Proxy

--- tools/ops/portainer/simple_start.sh
+++ tools/ops/portainer/simple_start.sh
@@ -7,8 +7,9 @@ docker run  --rm -d \
     -e 'LETSENCRYPT_HOST=portainer.myck.io' \
     -e 'VIRTUAL_HOST=portainer.myck.io' \
     -e 'VIRTUAL_PORT=9000' \
+    -v "/opt/portainer/data:/data" \
     -v "/var/run/docker.sock:/var/run/docker.sock" \
     -p 9000:9000 \
     portainer/portainer
 
-    #    -v "/opt/portainer/data:/data" \
+
