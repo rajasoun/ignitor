09ae72558386e56af4e5d55f613182428ad4f9b5
Raja. Soundaramourty <rajasoun@icloud.com>
2017-06-09 11:38:25 +0530

    [US13686] Nginx Refactoring using Proxy

--- tools/site/site.sh
+++ tools/site/site.sh
@@ -1,51 +1,16 @@
 #!/usr/bin/env sh
 
 option=$1
-composer="-f $(pwd)/tools/site/site.yml"
+composer="site.yml"
 DESC="Tracker Static Site"
 export STATIC_HOST=web.$(hostname)
 
-cleanup(){
-    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro spotify/docker-gc
+controller="ignitor.sh"
+getControllerPath() {
+    file=$1
+    WORKSPACE="$HOME/Workspace/ck/ignitor/"
+    controller=$(find $WORKSPACE -type f -name "*$file*")
 }
 
-
-setup_hostlocal(){
-    docker run --rm --privileged --net=host gliderlabs/hostlocal
-}
-
-
-
-set -e
-
-case "$option" in
-    setup)
-       echo -n "Setup $DESC "
-       setup_hostlocal #To Enable hostlocal.io
-    ;;
-
-    start)
-        echo -n "Starting $DESC: "
-        docker-compose $composer up -d  --build
-    ;;
-
-    stop)
-        echo -n "Stopping $DESC: "
-        docker-compose $composer down
-    ;;
-
-    teardown)
-        echo -n "TearDown $DESC: "
-        docker-compose $composer down
-        cleanup
-    ;;
-
-    log)
-        docker-compose $composer logs -f
-    ;;
-    *)
-        echo "Usage: ./site.sh {setup|start|stop|teardown|log}" >&2
-        exit 1
-    ;;
-esac
-exit 0
+getControllerPath $controller
+source $controller
