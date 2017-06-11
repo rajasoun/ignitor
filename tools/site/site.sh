2e76ee7b453a33ee948c69911a789e7ce6d7330a
Raja. Soundaramourty <rajasoun@icloud.com>
2017-06-12 01:15:34 +0530

    [US13686] Script Refactoring

--- tools/site/site.sh
+++ tools/site/site.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env sh
 
 option=$1
+DESC="Static Web Site"
+WORKSPACE="$HOME/Workspace/ck/ignitor/"
+
 composer="site.yml"
-DESC="Tracker Static Site"
-export STATIC_HOST=web.$(hostname)
+export SITE_HOST=$(hostname)
 
 controller="ignitor.sh"
 getControllerPath() {
     file=$1
-    WORKSPACE="$HOME/Workspace/ck/ignitor/"
     controller=$(find $WORKSPACE -type f -name "*$file*")
 }
 
-getControllerPath $controller
-source $controller
+setUp() {
+  getControllerPath $controller
+  # Source The Controller Script
+  . $controller
+}
+
+set -e
+setUp
+trap tearDown EXIT
