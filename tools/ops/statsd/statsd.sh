521dd7a31d7df4bfdae680e3ae3388d4ff82af51
Raja. Soundaramourty <rajasoun@icloud.com>
2017-09-19 11:36:11 +0530

    [US10233] Structural Refactoring

---
+++ tools/ops/statsd/statsd.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+option=$1
+DESC="StatsD Docker "
+WORKSPACE="$HOME/Workspace/ck/ignitor/"
+
+
+composer="statsd.yml"
+export PORTAINER_HOST=statsd.$(hostname)
+
+controller="ignitor.sh"
+getControllerPath() {
+    file=$1
+    controller=$(find $WORKSPACE -type f -name "*$file*")
+}
+
+
+setUp() {
+  getControllerPath $controller
+  # Source The Controller Script
+  . $controller
+}
+
+
+set -e
+setUp
+trap tearDown EXIT
+
